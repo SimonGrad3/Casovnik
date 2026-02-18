2da60cc368478d431cdf5ed6387d8000ba2ef4f1
Simon Grad <simon.grad123@gmail.com>
2026-02-18 13:11:54 +0100

    osnovne strani in premikanje po straneh

--- src/app/stran1/stran1.ts
+++ src/app/stran1/stran1.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MatAnchor } from "@angular/material/button";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stran1',
-  imports: [],
+  imports: [MatAnchor, FormsModule, CommonModule, MatCardModule, MatInputModule, MatButtonModule, MatIconModule, MatFormFieldModule],
   templateUrl: './stran1.html',
   styleUrl: './stran1.css',
 })
 export class Stran1 {
+  constructor(private router: Router) {}
 
+ levo() {
+    this.router.navigate(['/stran0']);
+  }
+
+  tabela() {
+    this.router.navigate(['/tabela']);
+  }
+
+  logout() {
+  localStorage.removeItem('currentUser'); 
+  this.router.navigate(['/login']); 
+}
 }
--- src/app/stranm1/stranm1.ts
+++ src/app/stranm1/stranm1.ts
@@ -1,11 +1,32 @@
+import { MatAnchor } from "@angular/material/button";
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stranm1',
-  imports: [],
+  imports: [MatAnchor, FormsModule, CommonModule, MatCardModule, MatInputModule, MatButtonModule, MatIconModule, MatFormFieldModule],
   templateUrl: './stranm1.html',
   styleUrl: './stranm1.css',
 })
 export class Stranm1 {
+  constructor(private router: Router) {}
 
+  desno() {
+    this.router.navigate(['/stran0']);
+  }
+  tabela() {
+    this.router.navigate(['/tabela']);
+  }
+
+  logout() {
+  localStorage.removeItem('currentUser'); 
+  this.router.navigate(['/login']); 
+}
 }
