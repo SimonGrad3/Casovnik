6b36c5a7721f9bd717dd851c4ffdee108a83fc5c
Simon Grad <simon.grad123@gmail.com>
2026-02-19 10:56:58 +0100

    Bonus: čas teče sproti

--- src/app/tabela/tabela.ts
+++ src/app/tabela/tabela.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core'; // 1. Dodan ChangeDetectorRef
 import { CommonModule } from '@angular/common';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { TimeTrackingService } from '../services/time-tracking';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-tabela',
@@ -15,20 +14,30 @@ import { Router } from '@angular/router';
   templateUrl: './tabela.html',
   styleUrl: './tabela.css',
 })
-export class Tabela implements OnInit {
+export class Tabela implements OnInit, OnDestroy {
 
   times: any = {};
+  private intervalSub?: Subscription;
 
-  constructor(private router: Router, private timeService: TimeTrackingService) {}
+  constructor(
+    private router: Router, 
+    private timeService: TimeTrackingService,
+    private cdr: ChangeDetectorRef 
+  ) {}
 
   ngOnInit() {
-
     this.loadTimes();
-
-    // LIVE update (bonus točka)
-    interval(1000).subscribe(() => {
-      this.loadTimes();
+    this.intervalSub = interval(1000).subscribe(() => {
+      this.loadTimes();   
+      // 3. PRISILIMO ANGULAR, DA OSVEŽI EKRAN!
+      this.cdr.detectChanges(); 
     });
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      this.intervalSub.unsubscribe();
+    }
   }
 
   loadTimes() {
@@ -39,13 +48,13 @@ export class Tabela implements OnInit {
     return Object.keys(this.times);
   }
 
-   stran0() {
+  stran0() {
     this.router.navigate(['/stran0']);
   }
 
   logout() {
-  this.timeService.stopTracking();
-  localStorage.removeItem('currentUser'); 
-  this.router.navigate(['/login']); 
-}
+    this.timeService.stopTracking();
+    localStorage.removeItem('currentUser'); 
+    this.router.navigate(['/login']); 
+  }
 }
